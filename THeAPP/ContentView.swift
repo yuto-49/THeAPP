169b0086f1e857dc5b29e70bd683265c52117133
MARU39 <ymaruyama1@hawk.iit.edu>
2024-07-16 10:24:40 +0900

    Initial Commit

---
+++ THeAPP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  THeAPP
+//
+//  Created by 丸山由羽斗 on 7/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ THeAPP/THeAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  THeAPPApp.swift
+//  THeAPP
+//
+//  Created by 丸山由羽斗 on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct THeAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
